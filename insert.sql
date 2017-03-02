ee227c9c668044297b226b6072d5feb320d765d3
Khoumal <henintsoakhoumal@gmail.com>
2017-03-02 14:53:23 +0300

    update 02032017

--- insert.sql
+++ insert.sql
@@ -1,78 +1,76 @@
-//insert into categorie(NOMCATEGORIE) values ("pack bomba");
-//insert into categorie(NOMCATEGORIE) values ("pack St Valentin");
-insert into categorie(NOMCATEGORIE) values ("Pizza");
-insert into categorie(NOMCATEGORIE) values ("Jus");
-insert into categorie(NOMCATEGORIE) values ("Snack");
-insert into categorie(NOMCATEGORIE) values ("Glaces");
+insert into categorie(NOMCATEGORIE) values ('Pizza');
+insert into categorie(NOMCATEGORIE) values ('Jus');
+insert into categorie(NOMCATEGORIE) values ('Snack');
+insert into categorie(NOMCATEGORIE) values ('Glaces');
 
 
 
 
 
-insert into produit(IDCATEGORIE,NOMPRODUIT,IMAGEPRODUIT) values (3,"pizza margherita","margherita.jpg");
-insert into produit(IDCATEGORIE,NOMPRODUIT,IMAGEPRODUIT) values (3,"pizza napolitaine","napolitaine.jpg");
-insert into produit(IDCATEGORIE,NOMPRODUIT,IMAGEPRODUIT) values (3,"pizza gastro","gastro.jpg");
-insert into produit(IDCATEGORIE,NOMPRODUIT,IMAGEPRODUIT) values (3,"pizza 4 fromages","4fromages.jpg");
-insert into produit(IDCATEGORIE,NOMPRODUIT,IMAGEPRODUIT) values (3,"pizza Jambon fromage","jambonfromage.jpg");
-insert into produit(IDCATEGORIE,NOMPRODUIT,IMAGEPRODUIT) values (3,"pizza exotique","exotique.jpg");
-insert into produit(IDCATEGORIE,NOMPRODUIT,IMAGEPRODUIT) values (3,"pizza royale","royale.jpg");
+insert into produit(IDCATEGORIE,NOMPRODUIT,IMAGEPRODUIT) values (5,'pizza margherita','margherita.jpg');
+insert into produit(IDCATEGORIE,NOMPRODUIT,IMAGEPRODUIT) values (5,'pizza napolitaine','napolitaine.jpg');
+insert into produit(IDCATEGORIE,NOMPRODUIT,IMAGEPRODUIT) values (5,'pizza gastro','gastro.jpg');
+insert into produit(IDCATEGORIE,NOMPRODUIT,IMAGEPRODUIT) values (5,'pizza 4 fromages','4fromages.jpg');
+insert into produit(IDCATEGORIE,NOMPRODUIT,IMAGEPRODUIT) values (5,'pizza Jambon fromage','jambonfromage.jpg');
+insert into produit(IDCATEGORIE,NOMPRODUIT,IMAGEPRODUIT) values (5,'pizza exotique','exotique.jpg');
+insert into produit(IDCATEGORIE,NOMPRODUIT,IMAGEPRODUIT) values (5,'pizza royale','royale.jpg');
 
-insert into produit(IDCATEGORIE,NOMPRODUIT,IMAGEPRODUIT) values (4,"Jus Naturel","jusnat.jpg");
-insert into produit(IDCATEGORIE,NOMPRODUIT,IMAGEPRODUIT) values (4,"Jus Star","star.jpg");
+insert into produit(IDCATEGORIE,NOMPRODUIT,IMAGEPRODUIT) values (6,'Jus Naturel','jusnat.jpg');
+insert into produit(IDCATEGORIE,NOMPRODUIT,IMAGEPRODUIT) values (6,'Jus Star','star.jpg');
 
-insert into produit(IDCATEGORIE,NOMPRODUIT,IMAGEPRODUIT) values (5,"Assiette Kebab","kebab.jpg");
-insert into produit(IDCATEGORIE,NOMPRODUIT,IMAGEPRODUIT) values (5,"Mi-sao","Mi-sao.jpg");
-insert into produit(IDCATEGORIE,NOMPRODUIT,IMAGEPRODUIT) values (5,"Soupe","Soupe.jpg");
-insert into produit(IDCATEGORIE,NOMPRODUIT,IMAGEPRODUIT) values (5,"Assiette Nem","Nem.jpg");
-insert into produit(IDCATEGORIE,NOMPRODUIT,IMAGEPRODUIT) values (5,"Frites","frites.jpg");
-insert into produit(IDCATEGORIE,NOMPRODUIT,IMAGEPRODUIT) values (5,"Sandwich","Sandwich.jpg");
-insert into produit(IDCATEGORIE,NOMPRODUIT,IMAGEPRODUIT) values (5,"hamburger","hamburger.jpg");
+insert into produit(IDCATEGORIE,NOMPRODUIT,IMAGEPRODUIT) values (7,'Assiette Kebab','kebab.jpg');
+insert into produit(IDCATEGORIE,NOMPRODUIT,IMAGEPRODUIT) values (7,'Mi-sao','Mi-sao.jpg');
+insert into produit(IDCATEGORIE,NOMPRODUIT,IMAGEPRODUIT) values (7,'Soupe','Soupe.jpg');
+insert into produit(IDCATEGORIE,NOMPRODUIT,IMAGEPRODUIT) values (7,'Assiette Nem','Nem.jpg');
+insert into produit(IDCATEGORIE,NOMPRODUIT,IMAGEPRODUIT) values (7,'Frites','frites.jpg');
+insert into produit(IDCATEGORIE,NOMPRODUIT,IMAGEPRODUIT) values (7,'Sandwich','Sandwich.jpg');
+insert into produit(IDCATEGORIE,NOMPRODUIT,IMAGEPRODUIT) values (7,'hamburger','hamburger.jpg');
 
-insert into produit(IDCATEGORIE,NOMPRODUIT,IMAGEPRODUIT) values (6,"Simple","simple.jpg");
-insert into produit(IDCATEGORIE,NOMPRODUIT,IMAGEPRODUIT) values (6,"Double","double.jpg");
-insert into produit(IDCATEGORIE,NOMPRODUIT,IMAGEPRODUIT) values (6,"Pot","pot.jpg");
-insert into produit(IDCATEGORIE,NOMPRODUIT,IMAGEPRODUIT) values (6,"Ice Poongy","poongy.jpg");
-
-
-
-
-insert into sexe(REFSEXE,NOMSEXE) values ("M","Masculin");
-insert into sexe(REFSEXE,NOMSEXE) values ("F","Feminin");
+insert into produit(IDCATEGORIE,NOMPRODUIT,IMAGEPRODUIT) values (8,'Simple','simple.jpg');
+insert into produit(IDCATEGORIE,NOMPRODUIT,IMAGEPRODUIT) values (8,'Double','double.jpg');
+insert into produit(IDCATEGORIE,NOMPRODUIT,IMAGEPRODUIT) values (8,'Pot','pot.jpg');
+insert into produit(IDCATEGORIE,NOMPRODUIT,IMAGEPRODUIT) values (8,'Ice Poongy','poongy.jpg');
 
 
 
 
-insert into client(IDSEXE,NOMCLIENT,DATENAISSANCE,ADRESSE) values (1,"Jean Paul","01/01/1960");
-insert into client(IDSEXE,NOMCLIENT,DATENAISSANCE,ADRESSE) values (1,"Koto Fetsy","24/05/1958");
-insert into client(IDSEXE,NOMCLIENT,DATENAISSANCE,ADRESSE) values (2,"Kala Ngita","04/04/1970");
-insert into client(IDSEXE,NOMCLIENT,DATENAISSANCE,ADRESSE) values (1,"Andrianina","02/05/1995");
-insert into client(IDSEXE,NOMCLIENT,DATENAISSANCE,ADRESSE) values (1,"Raoul","20/06/1995");
-insert into client(IDSEXE,NOMCLIENT,DATENAISSANCE,ADRESSE) values (1,"Rindra","01/11/1996");
-insert into client(IDSEXE,NOMCLIENT,DATENAISSANCE,ADRESSE) values (2,"Cah","20/11/1996");
-insert into client(IDSEXE,NOMCLIENT,DATENAISSANCE,ADRESSE) values (2,"Tian","07/10/1996");
-insert into client(IDSEXE,NOMCLIENT,DATENAISSANCE,ADRESSE) values (2,"Adri","10/08/1996");
+insert into sexe(REFSEXE,NOMSEXE) values ('M','Masculin');
+insert into sexe(REFSEXE,NOMSEXE) values ('F','Feminin');
 
 
 
-insert into prix(IDPRODUIT,MONTANT,DATEPRIX) values (1,16000,"01/03/2017");
-insert into prix(IDPRODUIT,MONTANT,DATEPRIX) values (2,16000,"01/03/2017");
-insert into prix(IDPRODUIT,MONTANT,DATEPRIX) values (3,16000,"01/03/2017");
-insert into prix(IDPRODUIT,MONTANT,DATEPRIX) values (4,16000,"01/03/2017");
-insert into prix(IDPRODUIT,MONTANT,DATEPRIX) values (5,16000,"01/03/2017");
-insert into prix(IDPRODUIT,MONTANT,DATEPRIX) values (6,16000,"01/03/2017");
-insert into prix(IDPRODUIT,MONTANT,DATEPRIX) values (7,16000,"01/03/2017");
-insert into prix(IDPRODUIT,MONTANT,DATEPRIX) values (8,2000,"01/03/2017");
-insert into prix(IDPRODUIT,MONTANT,DATEPRIX) values (9,3000,"01/03/2017");
-insert into prix(IDPRODUIT,MONTANT,DATEPRIX) values (10,8000,"01/03/2017");
-insert into prix(IDPRODUIT,MONTANT,DATEPRIX) values (11,8000,"01/03/2017");
-insert into prix(IDPRODUIT,MONTANT,DATEPRIX) values (12,10000,"01/03/2017");
-insert into prix(IDPRODUIT,MONTANT,DATEPRIX) values (13,7000,"01/03/2017");
-insert into prix(IDPRODUIT,MONTANT,DATEPRIX) values (14,2000,"01/03/2017");
-insert into prix(IDPRODUIT,MONTANT,DATEPRIX) values (15,8000,"01/03/2017");
-insert into prix(IDPRODUIT,MONTANT,DATEPRIX) values (16,6000,"01/03/2017");
-insert into prix(IDPRODUIT,MONTANT,DATEPRIX) values (17,2000,"01/03/2017");
-insert into prix(IDPRODUIT,MONTANT,DATEPRIX) values (18,3000,"01/03/2017");
-insert into prix(IDPRODUIT,MONTANT,DATEPRIX) values (19,1000,"01/03/2017");
-insert into prix(IDPRODUIT,MONTANT,DATEPRIX) values (20,500,"01/03/2017");
+
+insert into client(IDSEXE,NOMCLIENT,DATENAISSANCE,ADRESSE) values (1,'Jean Paul','01/01/1960','Meteo');
+insert into client(IDSEXE,NOMCLIENT,DATENAISSANCE,ADRESSE) values (1,'Koto Fetsy','24/05/1958','Ambanidia');
+insert into client(IDSEXE,NOMCLIENT,DATENAISSANCE,ADRESSE) values (1,'Andrianina','02/05/1995','Andranomena');
+insert into client(IDSEXE,NOMCLIENT,DATENAISSANCE,ADRESSE) values (1,'Raoul','20/06/1995','Meteo');
+insert into client(IDSEXE,NOMCLIENT,DATENAISSANCE,ADRESSE) values (1,'Rindra','01/11/1996','Dorodosy');
+insert into client(IDSEXE,NOMCLIENT,DATENAISSANCE,ADRESSE) values (2,'Cah','20/11/1996','67ha');
+insert into client(IDSEXE,NOMCLIENT,DATENAISSANCE,ADRESSE) values (2,'Tian','07/10/1996','Mahazoarivo');
+insert into client(IDSEXE,NOMCLIENT,DATENAISSANCE,ADRESSE) values (2,'Adri','10/08/1996','Ambohipo');
+insert into client(IDSEXE,NOMCLIENT,DATENAISSANCE,ADRESSE) values (2,'Kala Ngita','04/04/1970','Analakely');
+
+
+
+insert into prix(IDPRODUIT,MONTANT,DATEPRIX) values (2,16000,'01/03/2017');
+insert into prix(IDPRODUIT,MONTANT,DATEPRIX) values (3,16000,'01/03/2017');
+insert into prix(IDPRODUIT,MONTANT,DATEPRIX) values (4,16000,'01/03/2017');
+insert into prix(IDPRODUIT,MONTANT,DATEPRIX) values (5,16000,'01/03/2017');
+insert into prix(IDPRODUIT,MONTANT,DATEPRIX) values (6,16000,'01/03/2017');
+insert into prix(IDPRODUIT,MONTANT,DATEPRIX) values (7,16000,'01/03/2017');
+insert into prix(IDPRODUIT,MONTANT,DATEPRIX) values (8,16000,'01/03/2017');
+insert into prix(IDPRODUIT,MONTANT,DATEPRIX) values (9,2000,'01/03/2017');
+insert into prix(IDPRODUIT,MONTANT,DATEPRIX) values (10,3000,'01/03/2017');
+insert into prix(IDPRODUIT,MONTANT,DATEPRIX) values (11,8000,'01/03/2017');
+insert into prix(IDPRODUIT,MONTANT,DATEPRIX) values (12,8000,'01/03/2017');
+insert into prix(IDPRODUIT,MONTANT,DATEPRIX) values (13,10000,'01/03/2017');
+insert into prix(IDPRODUIT,MONTANT,DATEPRIX) values (14,7000,'01/03/2017');
+insert into prix(IDPRODUIT,MONTANT,DATEPRIX) values (15,2000,'01/03/2017');
+insert into prix(IDPRODUIT,MONTANT,DATEPRIX) values (16,8000,'01/03/2017');
+insert into prix(IDPRODUIT,MONTANT,DATEPRIX) values (17,6000,'01/03/2017');
+insert into prix(IDPRODUIT,MONTANT,DATEPRIX) values (18,2000,'01/03/2017');
+insert into prix(IDPRODUIT,MONTANT,DATEPRIX) values (19,3000,'01/03/2017');
+insert into prix(IDPRODUIT,MONTANT,DATEPRIX) values (20,1000,'01/03/2017');
+insert into prix(IDPRODUIT,MONTANT,DATEPRIX) values (21,500,'01/03/2017');
 
  
